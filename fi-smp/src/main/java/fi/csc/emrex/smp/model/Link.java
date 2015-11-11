fa63afba2f8ad8db809414a0dd2e1db76298dc02
Juho Pentikäinen <juho.pentikainen@gofore.com>
2015-11-11 13:16:18 +0200

    added getter to link

--- fi-smp/src/main/java/fi/csc/emrex/smp/model/Link.java
+++ fi-smp/src/main/java/fi/csc/emrex/smp/model/Link.java
@@ -1,11 +1,13 @@
 package fi.csc.emrex.smp.model;
 
+import lombok.Getter;
 import lombok.Setter;
 
 /**
  * Created by jpentika on 11/11/15.
  */
 @Setter
+@Getter
 public class Link {
     String link;
 }
