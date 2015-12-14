527a58b01ccbda571609aa51cc971384d052270e
salum-ar <salum.abdul-rahman@gofore.com>
2015-12-14 16:39:12 +0200

    jce workaround

--- fi-smp/src/main/java/fi/csc/emrex/smp/openpgp/PGPEncryptionUtil.java
+++ fi-smp/src/main/java/fi/csc/emrex/smp/openpgp/PGPEncryptionUtil.java
@@ -15,7 +15,6 @@ import org.bouncycastle.openpgp.PGPUtil;
 import org.bouncycastle.bcpg.ArmoredOutputStream;
 import org.bouncycastle.openpgp.*;
 
- 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.NoSuchProviderException;
@@ -23,6 +22,7 @@ import java.security.SecureRandom;
 import java.util.Date;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.util.Date;
@@ -33,74 +33,81 @@ import java.util.Date;
  * @author salum
  */
 public class PGPEncryptionUtil {
-    
-       private static final String BC_PROVIDER_NAME = "BC";
- 
+
+    private static final String BC_PROVIDER_NAME = "BC";
+
     // pick some sensible encryption buffer size
     private static final int BUFFER_SIZE = 4096;
- 
+
     // encrypt the payload data using AES-256,
     // remember that PGP uses a symmetric key to encrypt
     // data and uses the public key to encrypt the symmetric
     // key used on the payload.
     private static final int PAYLOAD_ENCRYPTION_ALG = PGPEncryptedData.AES_256;
- 
+
     // various streams we're taking care of
     private final ArmoredOutputStream armoredOutputStream;
     private final OutputStream encryptedOut;
     private final OutputStream compressedOut;
     private final OutputStream literalOut;
-    
-      public PGPEncryptionUtil(PGPPublicKey key, String payloadFilename, OutputStream out) throws PGPException, NoSuchProviderException, IOException {
- 
+
+    public PGPEncryptionUtil(PGPPublicKey key, String payloadFilename, OutputStream out) throws PGPException, NoSuchProviderException, IOException {
+        try {
+            Field field = Class.forName("javax.crypto.JceSecurity").
+                    getDeclaredField("isRestricted");
+            field.setAccessible(true);
+            field.set(null, java.lang.Boolean.FALSE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         // write data out using "ascii-armor" encoding.  This is the
         // normal PGP text output.
         this.armoredOutputStream = new ArmoredOutputStream(out);
- 
+
         // create an encrypted payload and set the public key on the data generator
         PGPEncryptedDataGenerator encryptGen = new PGPEncryptedDataGenerator(PAYLOAD_ENCRYPTION_ALG,
-                                                              new SecureRandom(), BC_PROVIDER_NAME);
+                new SecureRandom(), BC_PROVIDER_NAME);
         encryptGen.addMethod(key);
- 
+
         // open an output stream connected to the encrypted data generator
         // and have the generator write its data out to the ascii-encoding stream
         this.encryptedOut = encryptGen.open(armoredOutputStream, new byte[BUFFER_SIZE]);
- 
+
         // compress data.  we are building layers of output streams.  we want to compress here
         // because this is "before" encryption, and you get far better compression on
         // unencrypted data.
         PGPCompressedDataGenerator compressor = new PGPCompressedDataGenerator(PGPCompressedData.ZIP);
         this.compressedOut = compressor.open(encryptedOut);
- 
+
         // now we have a stream connected to a data compressor, which is connected to
         // a data encryptor, which is connected to an ascii-encoder.
         // into that we want to write a PGP "literal" object, which is just a named
         // piece of data (as opposed to a specially-formatted key, signature, etc)
         PGPLiteralDataGenerator literalGen = new PGPLiteralDataGenerator();
         this.literalOut = literalGen.open(compressedOut, PGPLiteralDataGenerator.UTF8,
-                                          payloadFilename, new Date(), new byte[BUFFER_SIZE]);
+                payloadFilename, new Date(), new byte[BUFFER_SIZE]);
     }
- 
+
     /**
      * Get an output stream connected to the encrypted file payload.
      */
     public OutputStream getPayloadOutputStream() {
         return this.literalOut;
     }
- 
+
     /**
      * Close the encrypted output writers.
      */
     public void close() throws IOException {
         // close the literal output
         literalOut.close();
- 
+
         // close the compressor
         compressedOut.close();
- 
+
         // close the encrypted output
         encryptedOut.close();
- 
+
         // close the armored output
         armoredOutputStream.close();
     }
@@ -109,11 +116,11 @@ public class PGPEncryptionUtil {
      * Decode a PGP public key block and return the keyring it represents.
      */
     public static PGPPublicKeyRing getKeyring(InputStream keyBlockStream) throws IOException {
-    // PGPUtil.getDecoderStream() will detect ASCII-armor automatically and decode it,
+        // PGPUtil.getDecoderStream() will detect ASCII-armor automatically and decode it,
         // the PGPObject factory then knows how to read all the data in the encoded stream
         PGPObjectFactory factory = new PGPObjectFactory(PGPUtil.getDecoderStream(keyBlockStream));
 
-    // these files should really just have one object in them,
+        // these files should really just have one object in them,
         // and that object should be a PGPPublicKeyRing.
         Object o = factory.nextObject();
         if (o instanceof PGPPublicKeyRing) {
@@ -130,7 +137,7 @@ public class PGPEncryptionUtil {
             return null;
         }
 
-    // iterate over the keys on the ring, look for one
+        // iterate over the keys on the ring, look for one
         // which is suitable for encryption.
         Iterator keys = keyRing.getPublicKeys();
         PGPPublicKey key = null;
