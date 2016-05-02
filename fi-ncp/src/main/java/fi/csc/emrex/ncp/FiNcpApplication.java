a0a3a7d1f31f3ee3eeb3a964315645150b147dc5
Salum <salum.abdul-rahman@gofore.com>
2016-05-03 00:22:08 +0300

    log web service requests

--- fi-ncp/src/main/java/fi/csc/emrex/ncp/FiNcpApplication.java
+++ fi-ncp/src/main/java/fi/csc/emrex/ncp/FiNcpApplication.java
@@ -27,6 +27,10 @@ public class FiNcpApplication {
     }
 
     public static void main(String[] args) {
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
         SpringApplication.run(FiNcpApplication.class, args);
     }
 }
